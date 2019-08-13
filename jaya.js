4af65d271f153fb5688f66f697d0ba1e3e2f505a
abhicherath <abhicherath@gmail.com>
2019-08-12 23:36:59 -0400

    Basic structure, decorations and map added.

--- jaya.js
+++ jaya.js
@@ -1,1 +1,12 @@
 console.log("Hello from JavaScript!");
+
+function initMap() {
+    // The location of Uluru
+    var apollo = {lat: 19.020603, lng: 73.029070};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 16, center: apollo});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: apollo, map: map});
+  }
+  
